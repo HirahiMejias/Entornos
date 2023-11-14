7689a4773d410a7e9d5a83bb321ae51dd64be765
1dama <1dama@informatica.edu>
2023-11-14 19:23:25 +0000

    Cambiamos Idioma a español

---
+++ gitgub.java
@@ -0,0 +1,5 @@
+public class gitgub {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hola,Mundo!");
+    }
+}
---
+++ github2.java
@@ -0,0 +1,5 @@
+public class github2 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hola Mundo");
+    }
+}
